ac64d387ea29757e3e109b8c54c40a237f213729
spirit <spirit.business15@gmail.com>
2026-02-19 18:31:20 +1100

    Update MixinGuiIngameHUD.java

--- src/main/java/myau/mixin/MixinGuiIngameHUD.java
+++ src/main/java/myau/mixin/MixinGuiIngameHUD.java
@@ -1,13 +1,13 @@
 package myau.mixin;
 
 import myau.ui.hud.ArraylistHUD;
-import net.minecraft.client.gui.GuiIngame;
+import net.minecraftforge.client.GuiIngameForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiIngame.class)
+@Mixin(GuiIngameForge.class)
 public class MixinGuiIngameHUD {
 
     private final ArraylistHUD hud = new ArraylistHUD();
