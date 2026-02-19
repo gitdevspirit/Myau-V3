45c7b0f428e900b579c1fea51b89c240d813f74e
spirit <spirit.business15@gmail.com>
2026-02-19 16:13:08 +1100

    Update MixinGuiIngameHUD.java

--- src/main/java/myau/mixin/MixinGuiIngameHUD.java
+++ src/main/java/myau/mixin/MixinGuiIngameHUD.java
@@ -1,6 +1,6 @@
 package myau.mixin;
 
-import myau.ui.hud.HUD;
+import myau.ui.hud.ArraylistHUD;
 import net.minecraft.client.gui.GuiIngame;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiIngame.class)
 public class MixinGuiIngameHUD {
 
-    private final ArraylistHUD hud = new HUD();
+    private final ArraylistHUD hud = new ArraylistHUD();
 
     @Inject(method = "renderGameOverlay", at = @At("RETURN"))
     private void renderHUD(float partialTicks, CallbackInfo ci) {
