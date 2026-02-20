95408997e02dff4c26270d6cbd6ecf2a126b9833
spirit <spirit.business15@gmail.com>
2026-02-20 22:09:02 +1100

    Refactor AntiObbyTrap to use BooleanSetting

--- src/main/java/myau/module/modules/AntiObbyTrap.java
+++ src/main/java/myau/module/modules/AntiObbyTrap.java
@@ -1,7 +1,7 @@
 package myau.module.modules;
 
+import myau.module.BooleanSetting;
 import myau.module.Module;
-import myau.property.properties.BooleanProperty;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
@@ -11,20 +11,20 @@ import net.minecraft.world.World;
 
 public class AntiObbyTrap extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
-    public final BooleanProperty setAir = new BooleanProperty("set-air", true);
+    public final BooleanSetting setAir = new BooleanSetting("Set Air", true);
 
     public AntiObbyTrap() {
         super("AntiObbyTrap", false);
+        register(setAir);
     }
 
     public boolean isInsideBlock(World world, BlockPos blockPos) {
         IBlockState blockState = world.getBlockState(blockPos);
         Block block = blockState.getBlock();
         if (block.getMaterial().isSolid() && block.isFullBlock()) {
-            Vec3 hitVec = new Vec3(mc.thePlayer.posX, mc.thePlayer.posY + (double) mc.thePlayer.getEyeHeight(), mc.thePlayer.posZ);
+            Vec3 hitVec = new Vec3(mc.thePlayer.posX, mc.thePlayer.posY + mc.thePlayer.getEyeHeight(), mc.thePlayer.posZ);
             return block.getCollisionBoundingBox(mc.theWorld, blockPos, blockState).isVecInside(hitVec);
-        } else {
-            return false;
         }
+        return false;
     }
 }
